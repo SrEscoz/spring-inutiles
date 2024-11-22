fa857c43ea606efd47fec198f3a92fbdadcc015e
Escoz <kyrovilches@gmail.com>
2024-11-22 11:44:45 +0100

    Commit inicial

---
+++ src/main/java/net/escoz/studentsbff/StudentsBffApplication.java
@@ -0,0 +1,13 @@
+package net.escoz.studentsbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/escoz/studentsbff/StudentsBffApplicationTests.java
@@ -0,0 +1,13 @@
+package net.escoz.studentsbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
